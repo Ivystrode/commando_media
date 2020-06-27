746abc2176c9b8885829267a5b8ceda14ea4d5cb
ben <benjamin200@hotmail.co.uk>
2020-06-27 20:39:34 +0100

    DT separate home JS make home tables neater on narrow screens

--- static/scripts/js/base.js
+++ static/scripts/js/base.js
@@ -77,14 +77,3 @@ function toProfile(username){
     window.location.assign("/profile/" + username)
 }
 
-function goToNotice(id){
-    window.location.href = 'notices/' + id
-}
-
-function goToIdea(id){
-    window.location.href = 'ideas/' + id
-}
-
-function goToAlbum(slug){
-    window.location.href = 'albums/' + slug
-}
---
+++ static/scripts/js/home.js
@@ -0,0 +1,11 @@
+function goToNotice(id){
+    window.location.href = 'notices/' + id
+}
+
+function goToIdea(id){
+    window.location.href = 'ideas/' + id
+}
+
+function goToAlbum(slug){
+    window.location.href = 'albums/' + slug
+}
